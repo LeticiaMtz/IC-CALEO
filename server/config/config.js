bdf4cb45a1ec1e7861a440437c144c529479e316
LeticiaMtz <leticiagpemoreno@gmail.com>
2021-12-06 13:51:14 -0600

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,16 @@
+
+process.env.PORT = process.env.PORT || 3000;
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//Conecciona  a la base de datos
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    log = true;
+    urlDB = 'mongodb+srv://admin:admin@cluster0.dyboa.mongodb.net/Estacionamiento?retryWrites=true&w=majority'; //mongodb://localhost:27017/estacionamiento
+} else {
+    urlDB = 'mongodb+srv://admin:admin@cluster0.dyboa.mongodb.net/Estacionamiento?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
+process.env.SEED = process.env.SEED || 'Frima-super-secreta';
+
+process.env.CADUCIDAD_TOKEN = process.env.CADUCIDAD_TOKEN || '3h';
---
+++ server/test/vehiculo.test.js
@@ -0,0 +1,20 @@
+const { getTags, getTag, postTag, putTag, deleteTag } = require('../routes/vehiculo');
+const assert = require('assert');
+
+describe('tags', function () {
+    it('How many parameter wait getTags function', function () {
+        assert.strictEqual(getTags.length, 1)
+    });
+    it('How many parameter wait getTags function', function () {
+        assert.strictEqual(getTag.length, 1)
+    });
+    it('How many parameter wait getTags function', function () {
+        assert.strictEqual(postTag.length, 7)
+    });
+    it('How many parameter wait getTags function', function () {
+        assert.strictEqual(putTag.length, 8)
+    });
+    it('How many parameter wait getTags function', function () {
+        assert.strictEqual(deleteTag.length, 2)
+    });
+})
